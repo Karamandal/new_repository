a5bd59294f649132359ec92121ea92fbe52b2da1
Karamandal <158753111+Karamandal@users.noreply.github.com>
2024-02-07 02:27:15 +0700

    add func Делает первые буквы заглавными (#5)

--- str_func.py
+++ str_func.py
@@ -1,3 +1,15 @@
 def uppercase_string(input_string):
-    """Делает буквы большими"""
+    """"Делает буквы заглавными"""
     return input_string.upper()
+
+def capitalize_words(input_string):
+    """"Делает первые буквы заглавными"""
+    words = input_string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
+
+string = "hello, world!"
+result2 = capitalize_words(string)
+result1 = uppercase_string(string)
+print(result1) #Вывод: HELLO, WORLD!
+print(result2) # Вывод: Hello, World!
